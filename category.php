230898dc184c8f3fb69109d7579a07ce5b719baf
VannyMiniTouch <VannyMiniTouch@gmail.com>
2022-08-12 17:44:22 +0700

    upfdate

---
+++ category.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+        <div class="frm">
+            <label for="">ID</label><br>
+            <input type="text" name="txt-id" id="txt-id" class="form-control"><br>
+            <label for="">Title</label><br>
+            <input type="text" name="txt-title" id="txt-title" class="form-control"><br>
+            <label for="">Description</label><br>
+            <textarea name="" id="" cols="30" rows="10"></textarea><br>
+            <label for="">Photo</label><br>
+            <input type="file" name="txt-file" id="txt-file" class="form-control"><br>
+            <label for="">Lang</label><br>
+            <select name="txt-lang" id="txt-lang" class="form-control"><br>
+                <option value="1">KH</option>
+                <option value="2">EN</option>
+            </select>
+            <label for="">Status</label>
+            <select name="txt-status" id="txt-status" class="form-control">
+                <option value="1">1</option>
+                <option value="2">2</option>
+            </select>
+        </div>
+</body>
+</html>
